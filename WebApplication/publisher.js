a25c837ed1d69199b04d3f373e95f29186d9fb82
Edwin Sukirno <esukirno@hotmail.com>
2014-05-04 21:34:29 +1000

    Added codes to repo

---
+++ WebApplication/publisher.js
@@ -0,0 +1,15 @@
+﻿var webApplication = webApplication || {};
+
+webApplication.publisher = function () {
+    var self = this;
+    this.message = ko.observable("");
+
+    this.notifySubscribers = function() {
+        $.event.trigger({
+            type : "messageUpdatedEvent",
+            message: { publishedMessage: self.message() },
+            time: new Date()
+        });
+    };
+
+};
---
+++ WebApplication/subscriber.js
@@ -0,0 +1,13 @@
+﻿var webApplication = webApplication || {};
+
+webApplication.subscriber = function () {
+    var self = this;
+    this.message = ko.observable("awaiting...");
+    this.timestamp = ko.observable("");
+
+    $(document).on("messageUpdatedEvent",
+        function (event) {
+            self.message(event.message.publishedMessage);
+            self.timestamp(event.time);
+        });
+};
